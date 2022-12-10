deeaf6828d51d956cd7dd81291735a70ccf264bf
NguyenBuiCao <120111755+NguyenBuiCao@users.noreply.github.com>
2022-12-10 15:49:58 +0700

    Add files via upload
    update front end, web route, controller

---
+++ Frontend_controler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+Use App\Http\Controllers\FrontendControler;
+use Illuminate\Http\Request;
+
+class Frontend_controler extends Controller
+{
+  public function index()
+  {
+    return view('inc/admin/shop');
+  }
+  public function product()
+  {
+    return view('inc/admin/product');
+  }
+  public function about()
+  {
+    return view('inc/admin/about');
+  }
+  public function single()
+  {
+    return view('inc/admin/single');
+  }
+  public function service()
+  {
+    return view('inc/admin/service');
+  }
+  public function contact()
+  {
+    return view('inc/admin/contact');
+  }
+}
